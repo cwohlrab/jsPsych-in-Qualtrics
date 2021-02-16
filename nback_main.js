d909bc41abba8992efd89165682f5073b0a7cb8f
cwohlrab <42815526+cwohlrab@users.noreply.github.com>
2021-02-16 17:01:26 -0600

    Add files via upload

---
+++ nback_main.js
@@ -0,0 +1,24 @@
+// Use JSDELIVR to get the files from a GitHub repository
+// https://cdn.jsdelivr.net/gh/<github-username>/<repository-name>/
+var repo_site = "https://cdn.jsdelivr.net/gh/cwohlrab/jsPsych-in-Qualtrics/n-back/";
+
+
+/* create timeline */
+var timeline = [];
+
+var n_back_set = ['Z', 'X', 'C', 'V', 'B', 'N'];
+var sequence = [];
+
+var how_many_back = 2;
+
+var sequence_length = 22;
+
+/* Instructions */
+var instructions_1 = {
+  type: 'html-button-response',
+  stimulus: '<div style="width: 800px;">'+
+    '<p>This experiment will test your ability to hold information in short-term, temporary memory. This is called working memory.</p>'+
+    '</div>',
+  choices: ["Continue"]
+}
+timeline.push(instructions_1);
