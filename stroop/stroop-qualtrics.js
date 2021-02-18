c7c6f3e93a09426828d3a9e3382ef8d5bc65c8f3
cwohlrab <42815526+cwohlrab@users.noreply.github.com>
2021-02-18 13:55:57 -0600

    Create stroop-qualtrics.js

---
+++ stroop/stroop-qualtrics.js
@@ -0,0 +1,168 @@
+Skip to content
+Search or jump to…
+
+Pull requests
+Issues
+Marketplace
+Explore
+ 
+@cwohlrab 
+cwohlrab
+/
+jsPsych-in-Qualtrics
+forked from kywch/jsPsych-in-Qualtrics
+0
+013
+Code
+Pull requests
+Actions
+Projects
+Wiki
+Security
+Insights
+Settings
+jsPsych-in-Qualtrics/n-back/stroop_main.js /
+@cwohlrab
+cwohlrab Create stroop_main.js
+Latest commit 94c2af1 31 seconds ago
+ History
+ 1 contributor
+125 lines (110 sloc)  3.83 KB
+  
+// Use JSDELIVR to get the files from a GitHub repository
+// https://cdn.jsdelivr.net/gh/<github-username>/<repository-name>/
+var repo_site = "https://cdn.jsdelivr.net/gh/cwohlrab/jsPsych-in-Qualtrics/stroop/";
+
+
+/* create timeline */
+var timeline = [];
+
+var n_back_set = ['Test', 'Hello', 'Good', 'Bad', 'Yes', 'No'];
+var sequence = [];
+
+var how_many_back = 2;
+
+var sequence_length = 22;
+
+/* Instructions */
+var instructions_1 = {
+  type: 'html-button-response',
+  stimulus: '<div style="width: 800px;">'+
+    '<p>This experiment will test your attention.</p>'+
+    '</div>',
+  choices: ["Continue"]
+}
+timeline.push(instructions_1);
+
+var instructions_2 = {
+  type: 'html-button-response',
+  stimulus: '<div style="width: 800px;">'+
+    '<p>You will see a sequence of letters presented one at a time. Your task is to determine if the letter on the screen matches '+
+    'the letter that appeared two letters before.</p>'+
+    '<p>If the letter is match <span style="font-weight: bold;">press the M key.</span></p>'+
+    '<p>For example, if you saw the sequence X, C, V, B, V, X you would press the M key when the second V appeared on the screen.</p>'+
+    '<p>You do not need to press any key when there is not a match.</p>'+
+    '</div>',
+  choices: ["Continue"]
+}
+timeline.push(instructions_2);
+
+var instructions_3 = {
+  type: 'html-button-response',
+  stimulus: '<div style="width: 800px;">'+
+    '<p>The sequence will begin on the next screen.</p>'+
+    '<p>Remember: press the M key if the letter on the screen matches the letter that appeared two letters ago.</p>'+
+    '</div>',
+  choices: ["I'm ready to start!"],
+  post_trial_gap: 1000
+}
+timeline.push(instructions_3);
+
+/* N Back sequence trials */
+var_trial = {
+  type: 'html-keyboard-response',
+  stimulus: function() {
+    if(sequence.length < how_many_back){
+      var letter = jsPsych.randomization.sampleWithoutReplacement(n_back_set, 1)[0]
+    } else {
+      if(jsPsych.timelineVariable('match', true) == true){
+        var letter = sequence[sequence.length - how_many_back];
+      } else {
+        var possible_letters = jsPsych.randomization.sampleWithoutReplacement(n_back_set, 2);
+        if(possible_letters[0] != sequence[sequence.length - how_many_back]){
+          var letter = possible_letters[0];
+        } else {
+          var letter = possible_letters[1];
+        }
+      }
+    }
+    sequence.push(letter);
+    return '<span style="font-size: 96px;">'+letter+'</span>'
+  },
+  choices: ['M'],
+  trial_duration: 1500,
+  response_ends_trial: false,
+  post_trial_gap: 500,
+  data: {
+    phase: 'test',
+    match: jsPsych.timelineVariable('match')
+  },
+  on_finish: function(data){
+    if(data.match == true){
+      data.correct = (data.key_press != null)
+    }
+    if(data.match == false){
+      data.correct = (data.key_press === null)
+    }
+  }
+}
+
+var n_back_trials = [
+  {match: true},
+  {match: false}
+]
+
+var n_back_sequence = {
+  timeline: [n_back_trial],
+  timeline_variables: n_back_trials,
+  sample: {
+    type: 'with-replacement',
+    size: sequence_length,
+    weights: [1, 2]
+  }
+}
+
+timeline.push(n_back_sequence);
+
+/* feedback */
+var feedback = {
+  type: 'html-keyboard-response',
+  stimulus: function(){
+    var test_trials = jsPsych.data.get().filter({phase: 'test'}).last(sequence_length-2);
+    var n_match = test_trials.filter({match: true}).count();
+    var n_nonmatch = test_trials.filter({match: false}).count();
+    var n_correct = test_trials.filter({match: true, correct: true}).count();
+    var false_alarms = test_trials.filter({match: false, correct: false}).count();
+
+    var html = "<div style='width:800px;'>"+
+      "<p>All done!</p>"+
+      "<p>You correctly identified "+n_correct+" of the "+n_match+" matching items.</p>"+
+      "<p>You incorrectly identified "+false_alarms+" of the "+n_nonmatch+" non-matching items as matches.</p>"
+
+    return html;
+  },
+  choices: jsPsych.NO_KEYS
+}
+timeline.push(feedback);
+© 2021 GitHub, Inc.
+Terms
+Privacy
+Security
+Status
+Docs
+Contact GitHub
+Pricing
+API
+Training
+Blog
+About
